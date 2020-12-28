9d0ed82813b542feed253b4dd47d8f1770db07c8
Alexey Rybakov <alexrybakov@yandex.ru>
2020-12-28 18:36:23 +0300

    429. More on useState() & State Updating

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+    const inputState = useState({
+        title: '',
+        amount: ''
+    });
+
     const submitHandler = event => {
         event.preventDefault();
         // ...
@@ -15,11 +20,33 @@ const IngredientForm = React.memo(props => {
                 <form onSubmit={submitHandler}>
                     <div className="form-control">
                         <label htmlFor="title">Name</label>
-                        <input type="text" id="title" />
+                        <input
+                            type="text"
+                            id="title"
+                            value={inputState[0].title}
+                            onChange={(event) => {
+                                const newValue = event.target.value;
+                                inputState[1]((prevInputState) => ({
+                                    title: newValue,
+                                    amount: prevInputState.amount
+                                }));
+                            }}
+                        />
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">Amount</label>
-                        <input type="number" id="amount" />
+                        <input
+                            type="number"
+                            id="amount"
+                            value={inputState[0].amount}
+                            onChange={(event) => {
+                                const newValue = event.target.value;
+                                inputState[1]((prevInputState) => ({
+                                    amount: newValue,
+                                    title: prevInputState.title
+                                }));
+                            }}
+                        />
                     </div>
                         <div className="ingredient-form__actions">
                         <button type="submit">Add Ingredient</button>
